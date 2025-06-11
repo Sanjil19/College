24b7ea858ffd23954614c2b15a815d08358f1315
Sanjil19 <sanjeel484@gmail.com>
2025-06-11 19:14:15 +0545

    first

---
+++ 2nd SEM/Javascript/day2/script.js
@@ -0,0 +1,61 @@
+// let i = 0;
+// for (i; i < 5; i++) {
+//   document.writeln("Sanjil Dahal");
+//   document.writeln("<br>");
+// }
+
+// let j = 10;
+// do {
+//   document.writeln("This is do while");
+//   document.writeln("<br>");
+
+//   j--;
+// } while (j != 0);
+
+// document.writeln("<br>");
+// document.writeln(`Sum of i and j is: ${i + j}`);
+// document.writeln("<br>");
+
+// num=5;
+// for(i=0;i<=10;i++){
+//     document.writeln(num*i);
+//     document.writeln("<br>");
+// }
+
+// let num = prompt("Enter a number to print its multiplication table");
+
+// let range= prompt('Enter the range for the multiplication table');
+
+// for (let i = 1; i <= range; i++) {
+//   document.writeln(`${num} x ${i} = ${num * i}`);
+//   document.writeln("<br>");
+// }
+
+// sum of digits
+
+// let num = prompt('Enter a number');
+// let sum=0;
+// while(num!=0){
+//     rem = num%10;
+//     sum=sum+rem;
+//     num=Math.floor(num/10);
+// }
+// document.writeln(sum);
+
+// let num = prompt("Enter which multiplication you want");
+// count = 0;
+// while (num != 0) {
+//   num = Math.floor(num / 10);
+//   count++;
+// }
+// document.writeln(count);
+
+// foreach
+// const a = ['apple','ball','cat'];
+
+// a.forEach(function(fruit)
+// {
+//     document.writeln(fruit);
+//     // document.writeln('<br>');
+// })
+
